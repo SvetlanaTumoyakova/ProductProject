313a9325ccd4f1a156c03a637645ae01596f6324
Светлана Тумоякова <Sveta.Tumoyakova@yandex.ru>
2025-07-14 18:01:42 +0700

    Add Model and Controller for Web Api

---
+++ ProductProjectAPI/Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStore.Model;
+using OnlineStore.Repositories.Interfaces;
+
+namespace OnlineStore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _productRepository;
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+        [HttpGet("{id}")]
+        public Product GetById(int id)
+        {
+            return _productRepository.GetById(id);
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> GetAll()
+        {
+            return _productRepository.GetAll();
+        }
+
+        [HttpGet("{skip}/{take}")]
+        public IEnumerable<Product> GetRange(int skip, int take)
+        {
+            return _productRepository.GetRange(skip, take);
+        }
+
+        [HttpPost]
+        public Product Add(Product product)
+        {
+            _productRepository.Add(product);
+            return product;
+        }
+
+        [HttpPut]
+        public void Update(Product editableProduct)
+        {
+            _productRepository.Update(editableProduct);
+        }
+
+        [HttpDelete("{id}")]
+        public void Remove(int id)
+        {
+            _productRepository.Remove(id);
+        }
+    }
+}
